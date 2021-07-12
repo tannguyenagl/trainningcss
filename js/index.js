df561d017ec4451c786f2540a08939eee7c34591
DESKTOP-7ARECON\AGL <tan10a7@gmail.com>
2021-07-12 18:53:46 +0700

    add responsive

--- js/index.js
+++ js/index.js
@@ -12,5 +12,40 @@ $('.c-voiceman__wrap').each(function (index, sliderWrap) {
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1204,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 620,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
+});
+
+$('.c-header__menu').click(function () {
+  $('.c-header__nav').css('left', '0px');
+  $('.c-header__close').css('left', '15px');
+});
+
+$('.c-header__close').click(function () {
+  $('.c-header__nav').css('left', '-100%');
+  $('.c-header__close').css('left', '-100%');
 });
